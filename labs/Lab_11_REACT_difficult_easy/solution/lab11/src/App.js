3cdfa36a55c6ebce22721484af0d825566e71936
MacsimNetsvetaev <macsimnetsvetaev@yandex.ru>
2023-12-26 02:27:46 +0300

    add lab 11

---
+++ labs/Lab_11_REACT_difficult_easy/solution/lab11/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './routes/Main'
+import About from './routes/About'
+import { useState } from 'react';
+
+function App() {
+
+  const [todoData, setData] = useState([
+    {
+        date: new Date(2023, 11, 27), 
+        tasks: ['Зачет по моему любимому предмету'], 
+        desc: ['пощады 🥺'], 
+        time: ['9:00'],
+    },
+])
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main todoData={todoData}/>}/>
+        <Route path='/about/:day/:year/:month' element={<About todoData={todoData} setData={setData}/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
