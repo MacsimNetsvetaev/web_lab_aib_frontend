04288eacd479a17975c17448dc88adab3856972e
MacsimNetsvetaev <macsimnetsvetaev@yandex.ru>
2023-12-26 12:01:27 +0300

    omg

--- labs/Lab_13_python_oop/Solution/main.py
+++ labs/Lab_13_python_oop/Solution/main.py
@@ -1,24 +1,22 @@
-from datetime import datetime
-import json
-import os
-from writer import XlsAnalyticPaymentWriter
 
-def load_data(file_path):
-    current_dir = os.path.dirname(__file__)
-    full_path = os.path.join(
-        current_dir,
-        file_path
-    )
-    with open(full_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
+from writer import XlsAnalyticPaymentWriter
+import json
+from datetime import datetime
+def read_file(file_name):
+    with open(file_name, "r", encoding="utf-8") as file:
+        some_data = json.load(file)
+        return some_data
+
+def main():
+    file_clients = '../clients.json'
+    file_payments = '../payments.json'
+    write_file =f"my_payments_analytics_{datetime.now().strftime('%Y-%m-%d')}.xlsx"
+    data_clients = read_file(file_clients)
+    data_payments = read_file(file_payments)
+    some_data = {'clients': data_clients['clients'], 'payments': data_payments['payments']}
+    analytic_writer = XlsAnalyticPaymentWriter(some_data)
+    analytic_writer.writer(write_file)
 
 if __name__ == '__main__':
-    data_clients = load_data('clients.json')
-    data_payments = load_data('payments.json')
-    data = {'clients': data_clients['clients'], 'payments': data_payments['payments']}
-    timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    output_file = f'Task1{timestamp}.xlsx'
-    xls_writer = XlsAnalyticPaymentWriter(data)
-    xls_writer.write_excel_report(output_file)
-    print(f"Данные сохранены в фийл: {output_file}")
+    main()
+    
